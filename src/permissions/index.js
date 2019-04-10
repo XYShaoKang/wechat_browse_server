f3294b91fd441a92102fb33ddbeb76e0b627d912
XYShaoKang <zengwenjie03@gmail.com>
2019-04-11 07:58:05 +0800

    fix: 修复认证范围

--- src/permissions/index.js
+++ src/permissions/index.js
@@ -9,6 +9,6 @@ export const permissions = shield({
     weChatUsers: isAuthenticatedUser,
   },
   Mutation: {
-    addWechatUsers: isAuthenticatedUser,
+    createWechatUsers: isAuthenticatedUser,
   },
 })
