bffb674be946d6e9540cb0e5441bc2475c9b14dc
XYShaoKang <zengwenjie03@gmail.com>
2019-04-22 04:24:40 +0800

    chore: 更新开发环境 支持直接在类定义中使用箭头函数 配置在 vscode 中更好的智能提示

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,20 @@
 const path = require('path')
 
+/**
+ * @param {string} relativePath
+ */
+const resolve = relativePath => path.resolve(__dirname, relativePath)
+
 module.exports = {
   verbose: true,
   roots: ['src'],
   testMatch: [path.join(__dirname, './src/__tests__/**/*.[jt]s?(x)')],
-  testPathIgnorePatterns: ['/node_modules/', '/__utils', '/__types'],
+  testPathIgnorePatterns: [
+    resolve('node_modules/'),
+    resolve('src/__tests__/__utils/'),
+    resolve('src/__tests__/__mocks/'),
+    resolve('src/__tests__/unit/__types.js'),
+  ],
   testEnvironment: 'node',
   coverageDirectory: './coverage/',
   collectCoverage: true,
